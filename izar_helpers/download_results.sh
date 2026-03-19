2a959f92a563485f7e2408c65deb0d666a4ee576
SalvatoreMastrangelo <salv.mastran@gmail.com>
2026-03-19 15:11:48 +0100

    more experiments + seed randomization

--- izar_helpers/download_results.sh
+++ izar_helpers/download_results.sh
@@ -1,1 +1,12 @@
-## TODO
+#!/bin/bash
+rsync -av \
+  --exclude='__pycache__' \
+  --exclude='*.pyc' \
+  --exclude='.venv' \
+  --exclude='venv' \
+  --exclude='.git' \
+  --exclude='*.egg-info' \
+  --exclude='**/.cache' \
+  --exclude='**/.mps' \
+  smastran@izar.hpc.epfl.ch:/home/smastran/genesis_runs/wp1_training \
+  logs/remote/
